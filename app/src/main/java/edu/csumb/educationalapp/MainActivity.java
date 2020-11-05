d876593119fd6566af8363c0b23b5bfa22de16f1
Cristian <carredondo@csumb.edu>
2020-11-04 20:02:18 -0800

    test commit arredondo

--- app/src/main/java/edu/csumb/educationalapp/MainActivity.java
+++ app/src/main/java/edu/csumb/educationalapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Testing Branch Arredondo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
