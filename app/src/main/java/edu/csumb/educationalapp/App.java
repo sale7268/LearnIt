22a67179410f676137c4b6a07ff13d2ebb395408
Cristian <carredondo@csumb.edu>
2020-11-07 10:46:11 -0800

    Fixed the database

--- app/src/main/java/edu/csumb/educationalapp/App.java
+++ app/src/main/java/edu/csumb/educationalapp/App.java
@@ -3,9 +3,11 @@ package edu.csumb.educationalapp;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
+
 import android.app.Application;
 
-class App extends Application {
+public class App extends Application {
 
     @Override
     public void onCreate() {
@@ -13,9 +15,13 @@ class App extends Application {
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId(getString(R.string.back4app_app_id))
+                // if defined
                 .clientKey(getString(R.string.back4app_client_key))
                 .server(getString(R.string.back4app_server_url))
                 .build()
         );
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
+
 }
