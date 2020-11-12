a149144c9ef3f624f1210c2bcf77889c6059ce0c
Cristian <carredondo@csumb.edu>
2020-11-11 18:52:06 -0800

    Able to add posts to database now.

--- app/src/main/java/edu/csumb/educationalapp/CreatePostActivity.java
+++ app/src/main/java/edu/csumb/educationalapp/CreatePostActivity.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class CreatePostActivity extends AppCompatActivity {
 
     EditText etTitle;
     EditText etContent;
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +29,29 @@ public class CreatePostActivity extends AppCompatActivity {
 
         etTitle = findViewById(R.id.etTitle);
         etContent = findViewById(R.id.etContent);
-    }
+        submitButton = findViewById(R.id.submitBtn);
 
-    void submitPost(View view){
-        if(TextUtils.isEmpty((etTitle.getText()))) {
-            etTitle.setError("Title is required");
-        } else if(TextUtils.isEmpty((etContent.getText()))) {
-            etContent.setError("Content is required");
-        }else{
-            ParseObject myPost = new ParseObject("Post");
-            myPost.put("title",etTitle.getText().toString().trim());
-            myPost.put("content",etContent.getText().toString().trim());
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-            myPost.saveInBackground();
+                if(TextUtils.isEmpty((etTitle.getText()))) {
+                    etTitle.setError("Title is required");
+                } else if(TextUtils.isEmpty((etContent.getText()))) {
+                    etContent.setError("Content is required");
+                }else{
+                    ParseObject myPost = new ParseObject("Post");
 
-            Toast.makeText(CreatePostActivity.this, "Post was submitted!", Toast.LENGTH_LONG).show();
-
-            Intent intent = new Intent(CreatePostActivity.this, HomeActivity.class);
-            startActivity(intent);
-        }
+                    myPost.put("title", etTitle.getText().toString().trim());
+                    myPost.put("content", etContent.getText().toString().trim());
+                    myPost.put("createdBy",ParseUser.getCurrentUser().getUsername());
+                    myPost.saveInBackground();
+                    Toast.makeText(CreatePostActivity.this,"Post was submitted!",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(CreatePostActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
     }
 }
